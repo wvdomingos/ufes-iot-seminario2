afa0186f47d574fa98a67da9ccdcaba7ecad1607
wbdomingos <wandervilhalvadomingos@gmail.com>
2021-08-23 20:31:40 -0400

    Update fontes

--- mqtt_pubs.sh
+++ mqtt_pubs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-numPublishers=1;
-qtdIoT=20
+numPublishers=$1;
+qtdIoT=$2
 simTime=60; # customize it
 for i in $(seq 1 $numPublishers);
  do
     nohup python mqtt_mininet.py $qtdIoT > pub_$i.log &
-    nohup bash LogaProcessamentoPS.sh $qtdIoT &
+    nohup bash LogaProcessamentoPS.sh $numPublishers &
 done 
 sleep "$simTime"
+gnuplot show_cpu_mem.plt
 killall python
 killall bash
